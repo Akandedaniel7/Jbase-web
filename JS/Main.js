744939c9099c30e520fd460449252e35258fa207
devsammyy <yinkaadebayo31@gmail.com>
2022-11-10 23:50:51 +0100

    fixed header issue

--- JS/Main.js
+++ JS/Main.js
@@ -1,36 +1,42 @@
 /* =============== HEADER  ============== */
-window.onscroll = function() {myFunction()};
+const nav = document.querySelector("nav");
+const a = document.querySelector("a");
 
+const sticky = nav.offsetTop;
+// alert(window.pageYOffset);
 
-var header = document.getElementById("header");
-
-var sticky = header.offsetTop;
-    function myFunction() {
-       if (window.pageYOffset > sticky) {
-        header.classList.add("scroll-header");
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset >= 200) {
+    nav.style.position = "fixed";
+    a.style.color = "black";
   } else {
-    header.classList.remove("scroll-header");
+    nav.style.position = "sticky";
+    a.style.color = "white";
   }
-}
-
-// /* =============== TYPE JS ============== */
-let typpe = new Typed('.autotype', {
-   strings: ["Blogger", "Developer", "Social Media Marketer", "Freelancer", "IT Support Manager"],
-   typeSpeed: 21,
-   backSpeed: 15,
-   loop: true
 });
 
-
+// /* =============== TYPE JS ============== */
+let typpe = new Typed(".autotype", {
+  strings: [
+    "Blogger",
+    "Developer",
+    "Social Media Marketer",
+    "Freelancer",
+    "IT Support Manager",
+  ],
+  typeSpeed: 21,
+  backSpeed: 15,
+  loop: true,
+});
 
 /* ============ SIDE MENU =========== */
 
-let sidemenu = document.getElementById('sidemenu');
+let sidemenu = document.getElementById("sidemenu");
 
-function openmenu(){
-	sidemenu.style.right = '0';
+function openmenu() {
+  sidemenu.style.right = "0";
 }
 
-function closemenu(){
-	sidemenu.style.right = '-100%';
+function closemenu() {
+  sidemenu.style.right = "-100%";
 }
